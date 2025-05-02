954ace74bd3ff2152dafe25395e7453f91dfe4a7
Joao Eduardo Luis <joao@clyso.com>
2025-05-02 21:39:40 +0000

    cbs: make it a pyproject, drop requirements.txt
    Signed-off-by: Joao Eduardo Luis <joao@clyso.com>

--- do-cbs-compose.sh
+++ do-cbs-compose.sh
@@ -2,5 +2,8 @@
 
 PODMAN_COMPOSE_PROVIDER="podman-compose" podman compose \
 	-f ./podman-compose.cbs.yaml down
+
+[[ "${1}" == "down" ]] && exit 0
+
 PODMAN_COMPOSE_PROVIDER="podman-compose" podman compose --verbose \
 	--podman-run-args="--rm" -f ./podman-compose.cbs.yaml up --build
