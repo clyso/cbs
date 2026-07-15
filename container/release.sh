fe9af56fa70383b5fbb415e8270e684a2b8903bd
Joao Eduardo Luis <joao@clyso.com>
2026-07-15 13:31:32 +0000

    release.sh: allow releasing only a 'cbc' version
    Signed-off-by: Joao Eduardo Luis <joao@clyso.com>

--- container/release.sh
+++ container/release.sh
@@ -36,6 +36,8 @@ Versions must always be in the format 'vMAJOR.minor.patch', where 'MAJOR',
 'minor', and 'patch', are integers greater or equal to zero.
 
 Options:
+  --cbc-only            Add tag for 'cbc' only (i.e., cbc-<version>)
+                        (implies '--no-update-latest')
   --no-update-latest    Do not update latest major version tag
   -r | --remote NAME    Name of remote to push to (default: ${default_remote_str})
   -h | --help           Show this message
@@ -43,12 +45,17 @@ Options:
 EOF
 }
 
+do_cbc_only=0
 update_latest_major=1
 push_remote="${default_remote}"
 positional_args=()
 
 while [[ $# -gt 0 ]]; do
   case "$1" in
+    --cbc-only)
+      do_cbc_only=1
+      update_latest_major=0
+      ;;
     --no-update-latest)
       update_latest_major=0
       ;;
@@ -115,6 +122,12 @@ version_regex='^v[0-9]+\.[0-9]+\.[0-9]+(-[a-z]+[0-9]+)?$'
 
 version_stream="$(echo "${version}" | cut -d'.' -f1)"
 
+release_tag="${version}"
+[[ ${do_cbc_only} -eq 1 ]] && {
+  release_tag="cbc-${version}"
+  echo -e "${_INFOMARK} creating tag '${release_tag}' for 'cbc' only"
+}
+
 echo -e "${_INFOMARK} update current main branch from remote '${push_remote}'"
 if ! git remote update "${push_remote}" >&/dev/null; then
   echo "error: unable to update remote '${push_remote}'" >&2
@@ -126,8 +139,8 @@ if ! git pull "${push_remote}" main:main >&/dev/null; then
   exit 1
 fi
 
-tag_found="$(git tag -l "${version}" 2>/dev/null)"
-[[ -n ${tag_found} && ${tag_found} == "${version}" ]] && {
+tag_found="$(git tag -l "${release_tag}" 2>/dev/null)"
+[[ -n ${tag_found} && ${tag_found} == "${release_tag}" ]] && {
   echo "error: version '${version}' already exists" >&2
   exit 1
 }
@@ -152,35 +165,44 @@ git_signing_key="$(git config user.signingkey 2>/dev/null)"
 }
 
 tmp_msg_file="$(mktemp)"
-cat <<EOF >"${tmp_msg_file}"
+
+if [[ ${do_cbc_only} -eq 0 ]]; then
+  cat <<EOF >"${tmp_msg_file}"
 Release CBS ${version}
 
 Signed-off-by: ${git_user} <${git_email}>
 EOF
+else
+  cat <<EOF >"${tmp_msg_file}"
+Release CBS CLI client (cbc) ${version}
 
-if ! git tag -s -F "${tmp_msg_file}" "${version}"; then
-  echo "error: unable to tag with version '${version}'"
+Signed-off-by: ${git_user} <${git_email}>
+EOF
+fi
+
+if ! git tag -s -F "${tmp_msg_file}" "${release_tag}"; then
+  echo "error: unable to tag with version '${release_tag}'"
   rm "${tmp_msg_file}"
   exit 1
 fi
 
 rm "${tmp_msg_file}"
 
-echo -e "${_CHECKMARK} created tag '${version}'"
+echo -e "${_CHECKMARK} created tag '${release_tag}'"
 echo -e "${_INFOMARK} pushing to remote '${push_remote}'..."
 
-if ! git push "${push_remote}" tag "${version}" >&/dev/null; then
-  echo "error: unable to push tag '${version}' to remote '${push_remote}'" >&2
+if ! git push "${push_remote}" tag "${release_tag}" >&/dev/null; then
+  echo "error: unable to push tag '${release_tag}' to remote '${push_remote}'" >&2
   exit 1
 fi
 
-echo -e "${_CHECKMARK} pushed tag '${version}' to '${push_remote}'"
+echo -e "${_CHECKMARK} pushed tag '${release_tag}' to '${push_remote}'"
 
 if [[ ${update_latest_major} -eq 1 ]]; then
   latest_major_tag="latest-${version_stream}"
-  echo -e "${_INFOMARK} updating latest major version tag '${latest_major_tag}' to '${version}'"
+  echo -e "${_INFOMARK} updating latest major version tag '${latest_major_tag}' to '${release_tag}'"
 
-  git tag -f "${latest_major_tag}" "${version}" >&/dev/null || {
+  git tag -f "${latest_major_tag}" "${release_tag}" >&/dev/null || {
     echo "error: unable to update latest major version tag '${latest_major_tag}'" >&2
     exit 1
   }
@@ -191,7 +213,7 @@ if [[ ${update_latest_major} -eq 1 ]]; then
   }
 
   echo -e "${_CHECKMARK} updated latest major version tag '${latest_major_tag}'" \
-    "to '${version}' and pushed to '${push_remote}'"
+    "to '${release_tag}' and pushed to '${push_remote}'"
 fi
 
 exit 0
